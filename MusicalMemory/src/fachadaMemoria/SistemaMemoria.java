3af4d58633c12fb95270df2998b40f2272a62d71
wsamarques@gmail.com <wsamarques@gmail.com@f7471ae6-18bc-4eff-0b78-1f2a23e05dd1>
2011-06-29 01:55:28 +0000

    Commitando a fachada.

--- MusicalMemory/src/fachadaMemoria/SistemaMemoria.java
+++ MusicalMemory/src/fachadaMemoria/SistemaMemoria.java
@@ -1,9 +1,18 @@
 package fachadaMemoria;
 
+import java.util.ArrayList;
+
+import dados.Som;
+import dados.Tabula;
+
+import excecoes.FaseInvalidaException;
 import excecoes.SomInvalidoException;
 import negocio.ControladorFases;
 import negocio.ControladorSom;
 
+/**
+ * Sistema controlador do jogo da memoria.
+ */
 public class SistemaMemoria {
 
 	private static SistemaMemoria instancia;
@@ -15,6 +24,75 @@ public class SistemaMemoria {
 		this.controleFases = ControladorFases.getControladorFases();
 	}
 	
+	/**
+	 * Usa o curinga referente ao filtro Passa-Alta.
+	 * 
+	 * @param somOriginal	- Som da tabula a ser filtrado.
+	 */
+	public void usarCuringaPassaAlta(Som somOriginal){
+		this.controleSom.aplicarPassaAlta(somOriginal);
+	}
+	
+	/**
+	 * Usa o curinga referente ao filtro Passa-Baixa.
+	 * 
+	 * @param somOriginal	- Som da tabula a ser filtrado.
+	 */
+	public void usarCuringaPassaBaixa(Som somOriginal){
+		this.controleSom.aplicarPassaBaixa(somOriginal);
+	}
+
+	/**
+	 * Usa o curinga referente ao filtro Echo.
+	 * 
+	 * @param somOriginal	- Som da tabula a ser filtrado.
+	 */
+	public void usarCuringaEcho(Som somOriginal){
+		this.controleSom.aplicarEcho(somOriginal);
+	}
+
+	/**
+	 * Usa o curinga referente ao filtro Sawtooth.
+	 * 
+	 * @param somOriginal	- Som da tabula a ser filtrado.
+	 */
+	public void usarCuringaSawtooth(Som somOriginal){
+		this.controleSom.aplicarSawtooth(somOriginal);
+	}
+
+	/**
+	 * Usa o curinga referente ao filtro Reverb.
+	 * 
+	 * @param somOriginal	- Som da tabula a ser filtrado.
+	 */
+	public void usarCuringaReverb(Som somOriginal){
+		this.controleSom.aplicarReverb(somOriginal);
+	}
+	
+	/**
+	 * Recupera o tabuleiro contendo as tabulas de cada fase.
+	 * 
+	 * @param fase						- Fase a qual se deseja obter
+	 * 									o tabuleiro.
+	 * 
+	 * @return							<code>ArrayList</code> de <code>Tabula</code>
+	 * 									representando o tabuleiro.
+	 * 
+	 * @throws FaseInvalidaException	Lancada caso a fase passada seja invalida.
+	 */
+	public ArrayList<Tabula> getTabuleiro(int fase) throws FaseInvalidaException{
+		return this.controleFases.getTabuleiro(fase);
+	}
+	
+	/**
+	 * Recupera a instancia atual do sistema.
+	 * 
+	 * @return							<code>SistemaMemoria</code> representando
+	 * 									o sistema controlador do jogo de memoria.
+	 * 
+	 * @throws SomInvalidoException		Lancada caso algum arquivo de som seja invalido
+	 * 									no momento do carregamento das musicas.
+	 */
 	public static SistemaMemoria getSistema() throws SomInvalidoException {
 		if (SistemaMemoria.instancia == null) {
 			SistemaMemoria.instancia = new SistemaMemoria();
