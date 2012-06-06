ef024b61c58b0ec7f39760b43e62374a1ef46e06
wellington.oj <wellington.oj@gmail.com>
2012-06-06 23:55:28 +0000

    Commit relacionado com as primeiras telas da gui.

---
+++ jogo2anca/serverside/noob/src/gui/TestMain.java
@@ -0,0 +1,14 @@
+package gui;
+
+import gui.frames.MainFrame;
+
+public class TestMain {
+	
+	public TestMain() {
+	
+		new MainFrame();
+	}
+	public static void main(String[] args) {
+		new TestMain();
+	}
+}
